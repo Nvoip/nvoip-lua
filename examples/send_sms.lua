3fe9f36c5b47b87b0830f268c68f686009aedb7b
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 17:31:17 -0300

    feat: add Lua send SMS example

---
+++ examples/send_sms.lua
@@ -0,0 +1,18 @@
+local nvoip = require("nvoip")
+
+local client = nvoip.new({
+  base_url = os.getenv("NVOIP_BASE_URL") or "https://api.nvoip.com.br/v2"
+})
+
+local oauth = client:create_access_token(
+  assert(os.getenv("NVOIP_NUMBERSIP"), "NVOIP_NUMBERSIP is required"),
+  assert(os.getenv("NVOIP_USER_TOKEN"), "NVOIP_USER_TOKEN is required")
+)
+
+local response = client:send_sms({
+  number_phone = os.getenv("NVOIP_TARGET_NUMBER") or "11999999999",
+  message = os.getenv("NVOIP_SMS_MESSAGE") or "Mensagem de teste Nvoip",
+  access_token = oauth.access_token
+})
+
+print(require("cjson.safe").encode(response))
